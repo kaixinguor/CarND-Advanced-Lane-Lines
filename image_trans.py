3912ad119d81e1f213db43e245f828414ccf9f57
kaixinguor <ling.wang.nj@gmail.com>
2017-02-21 00:34:56 +0100

    add gradient and color thresh

--- image_trans.py
+++ image_trans.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 from myutil import ensure_dir
-
+from matplotlib import pyplot as plt
 
 def undistort():
     path = 'output_images'
@@ -21,41 +21,65 @@ def undistort():
     cv2.imwrite(os.path.join(out_path, 'test1.jpg'), undistort_img)
 
 
+def gradient_thresh(img, orient='x', thresh=(0,255)):
+
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    if orient == 'x' or orient == 'y':
+        if orient == 'x':
+            sobel_img = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+        elif orient == 'y':
+            sobel_img = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+        abs_sobel = np.absolute(sobel_img)
+    elif orient == 'xy':
+        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+        abs_sobel = np.sqrt(sobelx**2+sobely**2)
+    else:
+        print("error")
+        return
+
+    scale_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
+    sbinary = np.zeros_like(gray)
+    sbinary[(scale_sobel >= thresh[0]) & (scale_sobel <= thresh[1])] = 1
+
+    return sbinary, abs_sobel, scale_sobel
+
+
+def hls_thresh(img, channel='s', thresh=(0,255)):
+
+    hls_img = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+
+    s_img = hls_img[:, :, 'hls'.find(channel)]
+    s_binary = np.zeros_like(s_img)
+    s_binary[(s_img>=thresh[0]) & (s_img<=thresh[1])] = 1
+    return s_binary, s_img
+
 if __name__ == '__main__':
 
+    image_file = 'test_images/test2.jpg'
+    ori_img = cv2.imread(image_file)
+    img = cv2.cvtColor(ori_img,cv2.COLOR_BGR2RGB) # convert to RGB
 
-    test_image = 'test_images/test2.jpg'
-    img = cv2.imread(test_image)
+    plt.subplot(1,3,1)
+    plt.imshow(img)
 
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    cv2.imshow("test",gray)
-    cv2.waitKey()
 
-    sobelx = cv2.Sobel(gray,cv2.CV_64F,1,0)
-    abs_sobelx = np.absolute(sobelx)
-    scale_sobelx = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0,1)
-    abs_sobely = np.absolute(sobely)
-    scale_sobely = np.uint8(255*abs_sobely/np.max(abs_sobely))
-
-    cv2.imshow("test",scale_sobelx)
-    cv2.waitKey()
-    cv2.imshow("test",scale_sobely)
-    cv2.waitKey()
-    print(np.max(abs_sobelx))
-
-    thresh_min = 20
-    thresh_max = 100
-    sxbinary = np.zeros_like(gray)
-    sxbinary[(scale_sobelx>=thresh_min) & (scale_sobelx<=thresh_max)] = 1
-    cv2.imshow("test", sxbinary)
-    cv2.waitKey()
     # compute gradient
+    sbinary, abs_sobel, scale_sobel = gradient_thresh(img,orient='x',thresh=(20,100))
+    print(np.max(abs_sobel))
+
+    plt.subplot(1,3,2)
+    plt.imshow(sbinary,cmap='gray')
 
 
     # color transform
-    hsl_img = cv2.cvtColor(img,cv2.COLOR_BGR2HLS)
-    s_img = hsl_img[:,:,2]
-    cv2.imshow("test",s_img)
-    cv2.waitKey()
+    hls_binary, s_img = hls_thresh(img, channel='s',thresh=(90,255))
+    print(s_img.shape)
+    print(hls_binary.shape)
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(hls_binary,cmap='gray')
+
+    plt.show()
 
